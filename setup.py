53fa967a5db5580b243a31ddd19e1d5b0d7e22b6
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2009-08-11 15:54:16 +0000

    Tagged 0.2.3
    git-svn-id: https://traduisons.googlecode.com/svn/trunk@39 fa4a6efc-d3b5-11dd-a412-a1792178251d

---
+++ setup.py
@@ -0,0 +1,30 @@
+from distutils import core
+
+# For side effects!
+import py2exe
+
+from py2exe import build_exe
+
+class Py2exeCommand(build_exe.py2exe):
+    def get_hidden_imports(self):
+        d = build_exe.py2exe.get_hidden_imports(self)
+        d.setdefault('gtk._gtk', []).extend([
+                'cairo', 'pango', 'pangocairo', 'atk'])
+        return d
+
+
+core.setup(
+    name = "Traduisons!",
+    description = "A front-end to Google Translate.",
+    version = "0.2.3",
+    console = [
+        {"script": "traduisons.py",
+        "icon_resources": [(1, "traduisons.png")]
+        }
+    ],
+#    options=opts,
+    data_files=[("traduisons.png")],
+    cmdclass={
+        'py2exe': Py2exeCommand,
+        },
+)
