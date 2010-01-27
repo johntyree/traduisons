72d4d02d19b955fdea94d70628b12d96868c41d8
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2010-01-27 11:06:15 +0000

    Final.. more or less.. translator class.
    git-svn-id: https://traduisons.googlecode.com/svn/branches/refactor@81 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- tdb.py
+++ tdb.py
@@ -26,11 +26,6 @@ try:
 except(ImportError):
     import simplejson as json
 
-def detectLang(text):
-    '''Return the guessed two letter code corresponding to text'''
-    urldata = urllib.urlencode({'v': 1.0, 'q': text})
-    response = urllib2.urlopen(urllib2.Request('http://ajax.googleapis.com/ajax/services/language/detect?%s' % urldata, None, {'User-Agent':'Traduisons/%s' % msg_VERSION})).read()
-    return json.loads(response)['responseData']['language']
 
 class translator:
     msg_VERSION = "0.3.3"
@@ -92,13 +87,20 @@ class translator:
                 'Yiddish' : 'yi',
                 }
 
-    def __init__(self, fromLang = 'auto', toLang = 'en', start_text = None):
-        self._fromLang = fromLang
-        self._toLang = toLang
+    def __init__(self, fromLang = 'auto', toLang = 'en', start_text = ''):
+        if not self.fromLang(fromLang): self.fromLang('auto')
+        if not self.toLang(toLang): self.toLang('en')
         self._text = start_text
 
+    def languages(self):
+        l = []
+        width = max([len(x) for x in self.dictLang.keys()])
+        for item in sorted(self.dictLang.keys()):
+            l.append(("%" + str(width) + 's' + ' : %s') % (item, self.dictLang[item]))
+        return '\n'.join(l)
+
     def toLang(self, l = None):
-        if l:
+        if l is not None:
             if l == 'auto': return False
             ## Check character code
             if l in self.dictLang.values(): self._toLang = l
@@ -108,63 +110,101 @@ class translator:
         return self._toLang
 
     def fromLang(self, l = None):
-        if l:
+        if l is not None:
             ## Check character code
             if l in self.dictLang.values():
-                self._toLang = l
+                self._fromLang = l
             else:
                 ## Check language name
-                self._toLang = self.dictLang.get(string.capitalize(l), self._toLang)
+                self._fromLang = self.dictLang.get(string.capitalize(l), self._fromLang)
         return self._fromLang
 
     def swapLang(self):
         f = self._fromLang
         t = self._toLang
-        if not self._toLang(f) or not self._fromLang(t):
-            self._toLang(t)
-            self._fromLang(f)
+        if not self.toLang(f) or not self.fromLang(t):
+            self._toLang = t
+            self._fromLang = f
             return False
         return True
 
-    def text(self, t = None):
-        if t:
+    def raw_text(self, t = None):
+        if t is not None:
             t = unicode(t)
             self._text = t
         return self._text
 
+    def text(self, text = None):
+        """Handle special cases of requested text."""
+        if text is None: return self._text
+        if text == '':
+            self._text = u''
+            return
+        RETURN_CODE = False
+        if text in ('.exit', '.quit', '.quitter', 'exit()'): RETURN_CODE = 'EXIT'
+        ## Use the '/' character to reverse translation direction.
+        elif text[0] == '/' or text[-1] == '/':
+            self.swapLang()
+            try:
+                # Cut off the '/' character if necessary
+                if text[-1] == '/': text = text[:-1]
+                elif text[0] == '/': text = text[1:]
+            except:
+                pass
+            self._text = text
+            RETURN_CODE = 'SWAP'
+        elif text in ('?', 'help', '-h', '-help', '--help'):
+            RETURN_CODE = 'HELP'
+        ## Use '|' character to change translation language(s).
+        elif text.find('|') + 1:
+            self.fromLang(text[0:text.find('|')])
+            self.toLang(text[text.find('|') + 1:])
+            RETURN_CODE = 'CHANGE'
+        else:
+            self._text = text
+        return (self._text, RETURN_CODE)
+
+    def detect_lang(self):
+        '''Return the guessed two letter code corresponding to text'''
+        urldata = urllib.urlencode({'v': 1.0, 'q': self._text})
+        response = urllib2.urlopen(urllib2.Request('http://ajax.googleapis.com/ajax/services/language/detect?%s' % urldata, None, {'User-Agent':'Traduisons/%s' % msg_VERSION})).read()
+        return json.loads(response)['responseData']['language']
+
     def translate(self):
         """Return translated text from fromLang to toLang."""
-        if self._text == None: return ''
-        try:
-            ## Use the official google translate-api via REST
-            ## 'auto' needs to be set to blank now
-            if self._fromLang == 'auto':
-                fromLangTemp = ''
-            else:
-                fromLangTemp = self._fromLang
-            urldata = urllib.urlencode({'v': 1.0,
-                                        'q': self._text,
-                                        'langpair' : '%s|%s' % (fromLangTemp, self._toLang)
-                                       })
-            url = 'http://ajax.googleapis.com/ajax/services/language/translate?%s' % (urldata)
-            headers = {'User-Agent':'Traduisons/%s' % self.msg_VERSION}
-            response = urllib2.urlopen(urllib2.Request(url, None, headers)).read()
-            translated_text = self._unquotehtml(json.loads(response)['responseData']['translatedText'])
+        if self._text == '':
+            self.result = ''
+        else:
+            try:
+                ## Use the official google translate-api via REST
+                ## 'auto' needs to be set to blank now
+                if self._fromLang == 'auto':
+                    fromLangTemp = ''
+                else:
+                    fromLangTemp = self._fromLang
+                urldata = urllib.urlencode({'v': 1.0,
+                                            'q': self._text,
+                                            'langpair' : '%s|%s' % (fromLangTemp, self._toLang)
+                                           })
+                url = 'http://ajax.googleapis.com/ajax/services/language/translate?%s' % (urldata)
+                headers = {'User-Agent':'Traduisons/%s' % msg_VERSION}
+                response = urllib2.urlopen(urllib2.Request(url, None, headers)).read()
+                self.result = self._unquotehtml(json.loads(response)['responseData']['translatedText'])
+            ##  If translated_text is empty (no translation found) handle exception.
+            except TypeError, e:
+                self._error = ('No translation available', e)
+                return False
+            ##  Not sure about this, some kind of error when the net is unavailable perhaps?
+            except urllib2.HTTPError, e:
+                self._error = ('urllib2.HTTPError', e)
+                return False
+            ## If the url ever changes...
+            except urllib2.URLError, e:
+                self._error = (e.reason, e)
+                return False
+        return True
 
-        ##  If translated_text is empty (no translation found) handle exception.
-        except TypeError, e:
-            self._error = ('No translation available', e)
-            translated_text = False
-        ##  Not sure about this, some kind of error when the net is unavailable perhaps?
-        except urllib2.HTTPError, e:
-            self._error = ('urllib2.HTTPError', e)
-            translated_text = False
-        ## If the url ever changes...
-        except urllib2.URLError, e:
-            self._error = (e.reason, e)
-        return translated_text
-
-    def _unquotehtml(s):
+    def _unquotehtml(self, s):
         """Convert a HTML quoted string into unicode object.
         Works with &#XX; and with &nbsp; &gt; etc."""
         def convertentity(m):
