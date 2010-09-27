c8ae47229ac729c90485eb6325b7e393d327fb0e
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2010-09-27 22:13:06 +0000

    First attempt at melding py2exe into setup.py
    git-svn-id: https://traduisons.googlecode.com/svn/branches/fix_distutils@127 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- setup.py
+++ setup.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 from distutils import core
 from traduisons.traduisons import msg_VERSION
+import sys
+
+Py2exeCommand = None
+if 'py2exe' in sys.argv:
+    # For side effects!
+    import py2exe
+    from py2exe import build_exe
+    class Py2exeCommand(build_exe.py2exe):
+        def get_hidden_imports(self):
+            d = build_exe.py2exe.get_hidden_imports(self)
+            d.setdefault('gtk._gtk', []).extend([
+                    'cairo', 'pango', 'pangocairo', 'atk'])
+            return d
 
 core.setup(
     name = "Traduisons",
@@ -17,4 +30,20 @@ core.setup(
                         'data/traduisons_icon.ico',
                         'data/google-small-logo.png']
     }
+
+    # py2exe data
+    windows = [
+        {"script": "traduisons.py",
+        "icon_resources": [(1, "traduisons_icon.ico")]
+        }
+    ],
+    options = {
+        'py2exe': {
+            'includes': ['gio'],
+        },
+    },
+    cmdclass={
+        'py2exe': Py2exeCommand,
+    },
 )
+
