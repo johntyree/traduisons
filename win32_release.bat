23060167119f059be425beb469a671ecc8a83141
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2010-09-27 23:58:27 +0000

    Manually copy ./traduisons/data into ./dist because py2exe doesn't honor package_data
    git-svn-id: https://traduisons.googlecode.com/svn/branches/fix_distutils@132 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- win32_release.bat
+++ win32_release.bat
@@ -1,5 +1,7 @@
 set /p VER=<LATEST-IS
 python setup.py py2exe
+mkdir dist\data
+copy /V traduisons\data dist\data
 makensis traduisons_installer.nsi
 copy traduisons_installer.exe Z:\traduisons_%VER%_win32.exe
 del /f /s /q traduisons_installer.exe build dist traduisons.pyc
