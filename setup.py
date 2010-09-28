aba2918ef2755d9ec4a6ce2ee00d931a45c55169
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2010-09-28 19:38:32 +0000

    Cleanup py2exe related errors and formatting.
    git-svn-id: https://traduisons.googlecode.com/svn/trunk@137 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
-from distutils import core
-from traduisons.traduisons import msg_VERSION
 import sys
+from distutils import core
+from traduisons import msg_VERSION
 
 Py2exeCommand = None
+py2exe_args = {}
+cmdclass_dict = {}
+
 if 'py2exe' in sys.argv:
     # For side effects!
     import py2exe
     from py2exe import build_exe
+    py2exe_args = {'windows':
+                       [{"script": "traduisons/traduisons.py",
+                         "icon_resources":
+                             [(1, "traduisons/data/traduisons_icon.ico")]}],
+                   'options': {'py2exe': {'includes': ['gio']}},
+        }
+    cmdclass_dict['py2exe'] = Py2exeCommand
+
     class Py2exeCommand(build_exe.py2exe):
         def get_hidden_imports(self):
             d = build_exe.py2exe.get_hidden_imports(self)
             d.setdefault('gtk._gtk', []).extend([
                     'cairo', 'pango', 'pangocairo', 'atk'])
             return d
+
 
 core.setup(
     name = "Traduisons",
@@ -30,19 +42,6 @@ core.setup(
                         'data/traduisons_icon.ico',
                         'data/google-small-logo.png']
     },
-    # py2exe data
-    windows = [
-        {"script": "traduisons/traduisons.py",
-        "icon_resources": [(1, "traduisons/data/traduisons_icon.ico")]
-        }
-    ],
-    options = {
-        'py2exe': {
-            'includes': ['gio'],
-        },
-    },
-    cmdclass={
-        'py2exe': Py2exeCommand,
-    },
+    cmdclass = cmdclass_dict,
+    **py2exe_args
 )
-
