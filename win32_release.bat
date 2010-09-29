894729439d85e231e07df4778f95fb3833cd34a9
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2010-09-29 16:46:01 +0000

    Another branch to fix distutils...
    git-svn-id: https://traduisons.googlecode.com/svn/branches/distutils@157 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- win32_release.bat
+++ win32_release.bat
@@ -1,9 +1,12 @@
+@echo off
 for /f "delims=" %%a in ('python setup.py --version') do @set VER=%%a
 python setup.py py2exe
 mkdir dist\data
 copy /V traduisons\data dist\data
 makensis traduisons_installer.nsi
-copy traduisons_installer.exe Z:\traduisons_%VER%_win32.exe
+@echo on
+copy traduisons_installer.exe Z:\traduisons-%VER%-win32.exe
+@echo off
 del /f /s /q build dist traduisons.pyc
 rmdir /s /q build dist
-echo "Copied: .\traduisons_installer.exe -> Z:\traduisons_%VER%_win32.exe"
+echo "Copied: .\traduisons_installer.exe -> Z:\traduisons-%VER%-win32.exe"
