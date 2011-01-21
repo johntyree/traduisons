90deb50c500515aaaf220d132d9fc435147e2804
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2011-01-21 00:01:34 +0000

    Import translator from package.
    git-svn-id: https://traduisons.googlecode.com/svn/trunk@212 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- src/gtkui.py
+++ src/gtkui.py
@@ -25,8 +25,7 @@
 """
 
 import base64
-import htmlentitydefs
-# In python <= 2.5, standard 'json' is not included 
+# In python <= 2.5, standard 'json' is not included
 try:
     import json
 except(ImportError):
@@ -40,6 +39,7 @@ import urllib
 import urllib2
 from distutils import version
 
+import traduisons
 
 msg_VERSION = version.StrictVersion('0.4.3')
 msg_DOWNLOAD = 'http://code.google.com/p/traduisons/downloads/list'
@@ -282,6 +282,7 @@ AAAABwAA/8AAAAAPAAD/4AAAAD8AAP/+AAA//wAA//+AAD//AAD//8AAf/8AAP//4AD//wAA///4
 Af//AAD///wP//8AAP///////wAA////////AAA=''', },
     }
 
+# decorator
 def echo(f):
     def newfunc(*args, **kwargs):
         print f.__name__, "BEGIN"
@@ -289,6 +290,7 @@ def echo(f):
         print f.__name__, "END"
     return newfunc
 
+# decorator
 def backgroundThread(f):
     echo = False
     if echo: print "backgroundThread definition start"
@@ -317,7 +319,7 @@ def backgroundThread(f):
     return newfunc
 
 def clipboard_get():
-    '''Return a gtk.Clipboard object or False if gtk in unavailable'''
+    '''Return a gtk.Clipboard object or False if gtk is unavailable'''
     try:
         import gtk
         class clipboard(gtk.Clipboard):
@@ -343,319 +345,9 @@ def clipboard_get():
         return False
 ## ------*------ End CLIPBOARD ------*------
 
-class translator:
-    '''Abstraction of the Google Translate RESTful API'''
-    dictLang = {'Detect Language': 'auto',
-                'Afrikaans': 'af',
-                'Albanian': 'sq',
-                'Arabic': 'ar',
-                'Basque': 'eu',
-                'Belarusian': 'be',
-                'Bulgarian': 'bg',
-                'Catalan': 'ca',
-                'Chinese': 'zh-CN',
-                'Chinese Simplified': 'zh-CN',
-                'Chinese Traditional': 'zh-TW',
-                'Croatian': 'hr',
-                'Czech': 'cs',
-                'Danish': 'da',
-                'Dutch': 'nl',
-                'English': 'en',
-                'Estonian': 'et',
-                'Filipino': 'tl',
-                'Finnish': 'fi',
-                'French': 'fr',
-                'Gaelic': 'ga',
-                'Galician': 'gl',
-                'German': 'de',
-                'Greek': 'el',
-                'Haitian Creole': 'ht',
-                'Hebrew': 'iw',
-                'Hindi': 'hi',
-                'Hungarian': 'hu',
-                'Icelandic': 'is',
-                'Indonesian': 'id',
-                'Irish': 'ga',
-                'Italian': 'it',
-                'Japanese': 'ja',
-                'Korean': 'ko',
-                'Latvian': 'lv',
-                'Lithuanian': 'lt',
-                'Macedonian': 'mk',
-                'Malay': 'ms',
-                'Maltese': 'mt',
-                'Norwegian': 'no',
-                'Persian': 'fa',
-                'Polish': 'pl',
-                'Portuguese': 'pt',
-                'Romanian': 'ro',
-                'Russian': 'ru',
-                'Serbian': 'sr',
-                'Slovak': 'sk',
-                'Slovenian': 'sl',
-                'Spanish': 'es',
-                'Swahili': 'sw',
-                'Swedish': 'sv',
-                'Thai': 'th',
-                'Turkish': 'tr',
-                'Ukrainian': 'uk',
-                'Vietnamese': 'vi',
-                'Welsh': 'cy',
-                'Yiddish': 'yi',
-                }
-    headers = {'User-Agent': 'Traduisons/%s' % (msg_VERSION,)}
-
-    def __init__(self, from_lang = 'auto', to_lang = 'en', start_text = ''):
-        if not self.from_lang(from_lang): self.from_lang('auto')
-        if not self.to_lang(to_lang): self.to_lang('en')
-        self._text = start_text
-
-    def is_latest(self):
-        '''Phone home to check if we are up to date.'''
-        try:
-            self.msg_LATEST
-        except AttributeError:
-            url = 'http://traduisons.googlecode.com/svn/trunk/LATEST-IS'
-            try:
-                ver = urllib2.urlopen(url).read().strip()
-            except urllib2.HTTPError:
-                return True
-            try:
-                self.msg_LATEST = version.StrictVersion(ver)
-            except ValueError:
-                pass
-        return msg_VERSION >= self.msg_LATEST
-
-    def update_languages(self):
-        '''
-        Naively try to determine if new languages are available by scraping
-        http://translate.google.com
-        '''
-        headers = self.headers
-        url = 'http://code.google.com/apis/language/translate/v2/using_rest.html'
-        req = urllib2.Request(url, None, headers)
-        resp = urllib2.urlopen(req).read()
-        regex = r'^\s+<td>([^<]*)</td>\n\s+<td><code>([^<]*)</code></td>'
-        name_code = re.findall(regex, resp, 8)
-        if name_code != []:
-            name_code = dict(name_code)
-            # These aren't listed, but are expected by users
-            name_code.update([('Detect Language', 'auto'),
-                             ('Chinese', 'zh-CN'),
-                             ('Gaelic', 'ga'),
-                             ('Korean', 'ko')])
-            # Remove any default languages that were not found
-            for k in self.dictLang:
-                if not name_code.has_key(k): print k, ': Unavailable'
-            self.dictLang = name_code
-        else:
-            print 'Unable to update_languages'
-        return False
-
-    def pretty_print_languages(self, right_justify = True):
-        '''
-        Return a string of pretty-printed, newline-delimited languages in
-        the format Name : code. "Detect Language : auto" is presented first.
-        '''
-        d = dict(self.dictLang) # force deep copy
-        d.pop('Detect Language')
-        l = ['Detect Language: auto']
-        width = 0
-        if right_justify:
-            width = max([len(x) for x in d.keys()])
-        for item in sorted(d.keys()):
-            line = ''.join(["%", str(width), 's: %s']) % \
-                (item, d[item])
-            l.append(line)
-        return '\n'.join(l)
-
-    def to_lang(self, l = None):
-        '''Get or set target language'''
-        if l is not None:
-            if l == 'auto':
-                return False
-            ## Check character code
-            if l in self.dictLang.values():
-                self._to_lang = l
-            else:
-                ## Check language name
-                self._to_lang = self.dictLang.get(string.capitalize(l),
-                                                  self._to_lang)
-        return self._to_lang
-
-    def from_lang(self, l = None):
-        '''Get or set source language.'''
-        if l is not None:
-            ## Check character code
-            if l in self.dictLang.values():
-                self._from_lang = l
-            else:
-                ## Check language name
-                self._from_lang = self.dictLang.get(string.capitalize(l),
-                                                    self._from_lang)
-        return self._from_lang
-
-    def swapLang(self):
-        '''Reverse direction the direction of translation.'''
-        f = self._from_lang
-        t = self._to_lang
-        if not self.to_lang(f) or not self.from_lang(t):
-            self._to_lang = t
-            self._from_lang = f
-            return False
-        return True
-
-    def raw_text(self, t = None):
-        '''
-        Get or set translation text, ignoring embedded directives such
-        as '/' and '.'.
-        '''
-        if t is not None:
-            t = unicode(t)
-            self._text = t
-        return self._text
-
-    def text(self, text = None):
-        '''
-        Get or set translation text, handling embedded directives such
-        as '/' and '.'.
-        '''
-        if text is None:
-            return self._text
-        if text == '':
-            self._text = u''
-            return
-        RETURN_CODE = False
-        if text in ('.exit', '.quit', '.quitter', 'exit()'):
-            RETURN_CODE = 'EXIT'
-        ## Use the '/' character to reverse translation direction.
-        elif text[0] == '/' or text[-1] == '/':
-            self.swapLang()
-            try:
-                # Cut off the '/' character if necessary
-                if text[-1] == '/': text = text[:-1]
-                elif text[0] == '/': text = text[1:]
-            except:
-                pass
-            self._text = text
-            RETURN_CODE = 'SWAP'
-        elif text in ('?', 'help', '-h', '-help', '--help'):
-            RETURN_CODE = 'HELP'
-        ## Use '|' character to change translation language(s).
-        elif text.find('|') + 1:
-            self.from_lang(text[0:text.find('|')])
-            self.to_lang(text[text.find('|') + 1:])
-            RETURN_CODE = 'CHANGE'
-        elif text in ('-v', '--version'):
-                RETURN_CODE = 'VERSION'
-        else:
-            self._text = text
-        return (self._text, RETURN_CODE)
-
-    def detect_lang(self):
-        '''
-        Return a tuple containing a two letter string, a boolean
-        "isReliable", and a confidence float corresponding to translation
-        text.
-        '''
-        urldata = urllib.urlencode({'v': 1.0, 'q': self._text})
-        url = 'http://ajax.googleapis.com/ajax/services/language/detect?%s' % \
-                (urldata,)
-        headers = self.headers
-        req = urllib2.Request(url, None, headers)
-        response = urllib2.urlopen(req).read()
-        result = json.loads(response)
-        if result['responseStatus'] != 200:
-            self._error = ('Unable to detect language',
-                           Exception(result['responseDetails']))
-            result['responseData'] = {'language': '',
-                                      'isReliable': False,
-                                      'confidence': 0}
-        return (result['responseData']['language'],
-                result['responseData']['isReliable'],
-                result['responseData']['confidence'])
-
-    def translate(self):
-        '''Return true if able to set self.result to translated text.'''
-        self.result = ''
-        if self._text == '':
-            return True
-        try:
-            # 'auto' needs to be set to blank now
-            if self._from_lang == 'auto':
-                from_lang_temp = ''
-            else:
-                from_lang_temp = self._from_lang
-            langpair = '%s|%s' % (from_lang_temp, self._to_lang)
-            urldata = urllib.urlencode({'v': 1.0,
-                                        'q': self._text,
-                                        'langpair': langpair,
-                                       })
-            url = 'http://ajax.googleapis.com/ajax/services/language/translate?%s' % \
-                (urldata,)
-            headers = self.headers
-            req = urllib2.Request(url, None, headers)
-            response = urllib2.urlopen(req).read()
-            result = json.loads(response)
-            if result['responseStatus'] != 200:
-                self._error = ('Unable to translate',
-                               Exception(result['responseDetails']))
-                translation = ''
-                return False
-            translation = result['responseData']['translatedText']
-            translation = self._unquotehtml(translation)
-            if translation.lower() == self._text.lower():
-                detected_lang = self.detect_lang()
-                # Prefer Dutch over Afrikaans
-                if detected_lang[0] == 'af' and detected_lang[1] == False:
-                    detected_lang[0] = 'nl'
-                    detected_lang[1] = False
-                    detected_lang[2] = 0
-                if detected_lang[0] == self.to_lang():
-                    if self.from_lang() != 'auto':
-                        self.swapLang()
-                        print "Reversing translation direction..."
-                        self.translate()
-                        translation = self.result
-        # If 'result' is empty (pretty generic error) handle exception.
-        except TypeError, e:
-            self._error = ('No translation available', e)
-            return False
-        except urllib2.HTTPError, e:
-            self._error = ('urllib2.HTTPError: Check network connection', e)
-            return False
-        ## If the url ever changes...
-        except urllib2.URLError, e:
-            self._error = (e.reason, e)
-            return False
-        finally:
-            try:
-                self.result = translation
-            except NameError:
-                pass
-        return True
-
-    def _unquotehtml(self, s):
-        '''Convert a HTML quoted string into unicode object.
-        Works with &#XX; and with &nbsp; &gt; etc.'''
-        def convertentity(m):
-            if m.group(1)=='#':
-                try:
-                    return chr(int(m.group(2)))
-                except XValueError:
-                    return '&#%s;' % (m.group(2),)
-            try:
-                return htmlentitydefs.entitydefs[m.group(2)]
-            except KeyError:
-                return ('&%s;' % (m.group(2),)).decode('ISO-8859-1'),
-        return re.sub(r'&(#)?([^;]+);',convertentity,s)
-
-## ------*------ End TRANSLATOR ------*------
-
-
 
 ## -----v----- BEGIN GUI -----v-----
-class TranslateWindow(translator):
+class TranslateWindow(traduisons.translator):
     '''Gui frontend to translate function.'''
     ## If gtk or pygtk fails to import, warn user and run at cli.
     try:
@@ -696,7 +388,7 @@ class TranslateWindow(translator):
             self.pixbufs[name] = loader.get_pixbuf()
 
 		## localize variables
-        translator.__init__(self, from_lang, to_lang)
+        traduisons.translator.__init__(self, from_lang, to_lang)
 
         ## Generate user messages
         self.msg_LANGTIP  = self.pretty_print_languages(0)
@@ -933,7 +625,7 @@ def main():
         gtk.main()
     else:
         print "\nTraduisons! - %s\npowered by Google ..." % (msg_VERSION,)
-        t = translator()
+        t = traduisons.translator()
         if not t.is_latest():
             print "Version %s now available! %s" % (t.msg_LATEST,
                                                     msg_DOWNLOAD)
