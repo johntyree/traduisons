6de9c059de7bca50cdf6fdc50df7087750c054e0
johntyree <johntyree@fa4a6efc-d3b5-11dd-a412-a1792178251d>
2011-11-26 00:09:48 +0000

    Cleanup from merge.
    git-svn-id: https://traduisons.googlecode.com/svn/branches/svn_refactor@250 fa4a6efc-d3b5-11dd-a412-a1792178251d

--- traduisons/__init__.py
+++ traduisons/__init__.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 #
 #       traduisons/__init__.py
-#       
-<<<<<<< HEAD
+#
 #       Copyright 2011 John Tyree <johntyree@gmail.com>
-=======
-#       Copyright 2010 John Tyree <johntyree@gmail.com>
->>>>>>> master
-#       
+#
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
-#       
+#
 #       This program is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
-#       
+#
 #       You should have received a copy of the GNU General Public License
 #       along with this program; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
@@ -25,7 +21,4 @@
 
 
 from traduisons import *
-<<<<<<< HEAD
 import gtkui
-=======
->>>>>>> master
